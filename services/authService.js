f62dbb1cd2146032e59ffacaf0bb0f3cd7a0e5e9
Zarbba <zarbba@hotmail.com>
2024-10-31 16:04:06 +1100

    Completes authService.js.

--- services/authService.js
+++ services/authService.js
@@ -1,15 +1,40 @@
 const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL
+const TOKEN_KEY = import.meta.env.VITE_JWT_KEY
 
-const signup = async (inputData) => {
+const signUp = async (input) => {
 	try {
 		const res = await fetch(`${BACKEND_URL}/Auth/Signup`, {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(inputData),
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(input),
 		})
-		const json = await res.json()
-		if (json.err) {
-			throw new Error(json.err)
+		const output = await res.json()
+		if (output.err) {
+			throw new Error(output.err)
+		}
+		localStorage.setItem('JWT', output.token)
+		return output
+	} catch (err) {
+		console.log(err)
+		throw err
+	}
+}
+
+const signIn = async (input) => {
+	try {
+		const res = await fetch(`${BACKEND_URL}/Auth/Signin`, {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(input),
+		})
+		const output = await res.json()
+		if (output.err) {
+			throw new Error(output.err)
+		}
+		if (output.token) {
+			localStorage.setItem(TOKEN_KEY, output.token)
+			const user = JSON.parse(atob(output.token.split('.')[1]))
+			return user
 		}
 	} catch (err) {
 		console.log(err)
@@ -17,12 +42,18 @@ const signup = async (inputData) => {
 	}
 }
 
-const signin = async () => {
-	console.log("signin test")
+const getUser = () => {
+	const token = localStorage.getItem(TOKEN_KEY)
+
+	if (!token) {
+		return null
+	}
+	const user = JSON.parse(atob(token.split('.')[1]))
+	return user
 }
 
-const getUser = async () => {
-	console.log("getUser test")
+const signOut = () => {
+	localStorage.removeItem('JWT')
 }
 
-export { signup, signin, getUser }
+export {signUp, signIn, getUser, signOut}
