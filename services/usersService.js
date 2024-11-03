2d1b0bac479bc40b04e59640efabb916c138bb87
Eddie <edwardchen_97@hotmail.com>
2024-11-03 20:36:38 +1100

    SLSP-5

--- services/usersService.js
+++ services/usersService.js
@@ -1,16 +1,80 @@
 const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL
-const TOKEN_KEY = import.meta.env.VITE_JWT_KEY
 
-//* This functions gets all the info of the user
-const userProfile = async () => {
+const getProfile = async () => {
 	try {
-		const response = await fetch('BACKEND_URL/users/:userId')
-        const data = await response.json()
-        console.log(data)
-		return data
+		const res = await fetch(`${BACKEND_URL}`, {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+		})
+		return res.json()
 	} catch (error) {
-		console.log(error)
+		console.log('Error getting your profile', error)
+		return {error: 'Error getting your profile!'}
 	}
 }
 
-export {userProfile}
+const createList = async (userId, ListFormData) => {
+	try {
+		const res = await fetch(`${BACKEND_URL}/${userId}/lists`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(ListFormData),
+		})
+		return res.json()
+	} catch (error) {
+		console.error('Error creating list:', error)
+		return {error: 'Error creating list'}
+	}
+}
+
+const showList = async (userId, listId) => {
+	try {
+		const res = await fetch(`${BACKEND_URL}/${userId}/lists/${listId}`, {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+		})
+		return res.json()
+	} catch (error) {
+		console.error('Error getting list:', error)
+		return {error: 'Error getting list '}
+	}
+}
+
+const updateList = async (userId, listId, ListFormData) => {
+	try {
+		const res = await fetch(`${BACKEND_URL}/${userId}/lists/${listId}`, {
+			method: 'PUT',
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(ListFormData),
+		})
+		return res.json()
+	} catch (error) {
+		console.error('Error updating list:', error)
+		return {error: `Error updating list `}
+	}
+}
+
+const deleteList = async (userId, listId) => {
+	try {
+		const res = await fetch(`${BACKEND_URL}/${userId}/lists/${listId}`, {
+			method: 'DELETE',
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+		})
+		return res.json()
+	} catch (error) {
+		console.error('Error deleting list:', error)
+		return {error: `Error deleting list`}
+	}
+}
+
+export {getProfile, createList, showList, updateList, deleteList}
