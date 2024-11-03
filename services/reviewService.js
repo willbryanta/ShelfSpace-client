b232a5506a72edaec8a08c731db8a177608c87f5
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-11-03 15:42:55 +1100

    edit: I believe this is all we need for the review service, as request validation is handled server side

--- services/reviewService.js
+++ services/reviewService.js
@@ -1,15 +1,21 @@
 const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL
 const TOKEN_KEY = import.meta.env.VITE_JWT_KEY
 
-const createReview = async () => {
+// Create a review
+const createReview = async (input) => {
 	try {
-		const res = await fetch(`${BACKEND_URL}/reviews`)
+		const res = await fetch(`${BACKEND_URL}/reviews`, {
+			method: 'POST',
+			headers: {'Content-type': 'application/json'},
+			body: JSON.stringify(input),
+		})
 		const data = await res.json()
+		if (data.err) {
+			throw new Error(data.err)
+		}
 		return data
 	} catch (error) {
 		console.log(error)
 		throw error
 	}
 }
-
-export {createReview}
