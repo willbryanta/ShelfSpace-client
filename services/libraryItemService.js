4991b5ac05f34da3d11d29056c2943cf7e6e5a28
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-11-04 22:24:20 +1100

    edit: added get library item service, might need to add a service to get individual library items as well - TBD

--- services/libraryItemService.js
+++ services/libraryItemService.js
@@ -1,7 +1,28 @@
 const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL
 const TOKEN_KEY = import.meta.env.VITE_JWT_KEY
 
-// Create a LibraryItem
+const getLibraryItem = async () => {
+	try {
+		const res = await fetch(`${BACKEND_URL}/library`, {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+		})
+
+		if (!res.ok) {
+			const errorData = await res.json()
+			throw new Error(errorData.err)
+		}
+
+		const data = await res.json()
+		return data
+	} catch (error) {
+		console.error(error)
+		throw error
+	}
+}
+
 const createLibraryItem = async (libraryItem) => {
 	try {
 		const res = await fetch(`${BACKEND_URL}/library`, {
@@ -53,4 +74,4 @@ const updateLibraryItem = async (libraryItemId, libraryItemFormData) => {
 	}
 }
 
-export {createLibraryItem, deleteLibraryItem, updateLibraryItem}
+export {createLibraryItem, getLibraryItem, deleteLibraryItem, updateLibraryItem}
