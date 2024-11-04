e4184dd3c8fdea0b5f799068de6c511eb9f293a8
Zarbba <zarbba@hotmail.com>
2024-11-04 23:11:40 +1100

    Feat: Improves format and layout of profile page.

--- components/ProfilePage/ProfilePage.jsx
+++ components/ProfilePage/ProfilePage.jsx
@@ -3,7 +3,7 @@ import * as usersService from '../../services/usersService'
 import ListIndexDisplay from '../ListIndexDisplay/ListIndexDisplay'
 import ReviewDisplay from '../ReviewDisplay/ReviewDisplay'
 import UserSettings from '../UserSettings/UserSettings'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 function ProfilePage(props) {
 	const {user, handleSetUser, authService} = props
 	const {getProfile, updateUser} = usersService
@@ -19,20 +19,30 @@ function ProfilePage(props) {
 	}, [])
 	return (
 		<>
-			<ul></ul>
-			{lists.map((list) => {
-				return (
-					<li key={list._id}>
-						<ListIndexDisplay key={list._id} list={list} user={user} />
-					</li>
-				)
-			})}
+			<h3>Your Lists</h3>
+			<ul>
+				{lists.map((list) => {
+					return (
+						<li key={list._id}>
+							<ListIndexDisplay key={list._id} list={list} user={user} />
+						</li>
+					)
+				})}
+			</ul>
+			<h3>Your Reviews</h3>
 			<ul>
 				{reviews.map((review) => {
 					return (
 						<li key={review._id}>
-							<p><Link to={`/library/${review.libraryItem._id}`}>{review.libraryItem.name} </Link>Review:</p>
 							<ReviewDisplay review={review} />
+							<p>
+								<em>
+									on{' '}
+									<Link to={`/library/${review.libraryItem._id}`}>
+										{review.libraryItem.name}
+									</Link>
+								</em>
+							</p>
 						</li>
 					)
 				})}
