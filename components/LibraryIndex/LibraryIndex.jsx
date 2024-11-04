fd05a553fe6495530ab66d0a8600e49bfbdc7ade
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-11-05 10:35:31 +1100

    create: index for all library items

--- components/LibraryIndex/LibraryIndex.jsx
+++ components/LibraryIndex/LibraryIndex.jsx
@@ -1,5 +1,21 @@
-function LibraryIndex() {
-	return <></>
+function LibraryIndex(props) {
+	const {libraryItems} = props
+
+	const allLibraryItems = libraryItems.map((libraryItem, index) => (
+		<ul key={index}>
+			<li>Name: {libraryItem.name}</li>
+			<li>Description: {libraryItem.description}</li>
+			<li>Publication Date: {libraryItem.publicatioDate}</li>
+			<li>Author: {libraryItem.author}</li>
+			<li>Reviews: {libraryItem.reviews}</li>
+		</ul>
+	))
+
+	return (
+		<>
+			<div>{allLibraryItems}</div>
+		</>
+	)
 }
 
 export default LibraryIndex
