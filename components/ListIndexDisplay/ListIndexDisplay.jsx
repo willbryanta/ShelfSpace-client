c3369e3df6995144460cf7c15c682bf0f61eca91
Zarbba <zarbba@hotmail.com>
2024-11-04 20:38:46 +1100

    Feat: Continues implementation of user profile. Further work moved to other cards.

--- components/ListIndexDisplay/ListIndexDisplay.jsx
+++ components/ListIndexDisplay/ListIndexDisplay.jsx
@@ -1,5 +1,13 @@
-function ListIndexDisplay() {
-	return <><h1>TEST</h1></>
+import {Link} from 'react-router-dom'
+
+function ListIndexDisplay(props) {
+	const {user, list} = props
+	const {listName, _id} = list
+	return (
+		<>
+			<Link to={`/users/${user._id}/lists/${_id}`}>{listName}</Link>
+		</>
+	)
 }
 
 export default ListIndexDisplay
--- components/ProfilePage/ProfilePage.jsx
+++ components/ProfilePage/ProfilePage.jsx
@@ -3,15 +3,16 @@ import * as usersService from '../../services/usersService'
 import ListIndexDisplay from '../ListIndexDisplay/ListIndexDisplay'
 import ReviewDisplay from '../ReviewDisplay/ReviewDisplay'
 import UserSettings from '../UserSettings/UserSettings'
+import { Link } from 'react-router-dom'
 function ProfilePage(props) {
 	const {user, handleSetUser, authService} = props
 	const {getProfile, updateUser} = usersService
 	const [lists, setLists] = useState([])
 	const [reviews, setReviews] = useState([])
 	const generateProfile = async () => {
-		const profile = await getProfile(user)
-		setLists(profile.lists)
-		setReviews(profile.ownedReviews)
+		const profileData = await getProfile(user)
+		setLists(profileData.user.lists)
+		setReviews(profileData.reviews)
 	}
 	useEffect(() => {
 		generateProfile()
@@ -22,16 +23,16 @@ function ProfilePage(props) {
 			{lists.map((list) => {
 				return (
 					<li key={list._id}>
-						<ListIndexDisplay key={list._id} list={list} />
+						<ListIndexDisplay key={list._id} list={list} user={user} />
 					</li>
 				)
 			})}
 			<ul>
-				{reviews.map((item) => {
+				{reviews.map((review) => {
 					return (
-						<li key={item.review._id}>
-							<p>{item.filmTitle} Review:</p>
-							<ReviewDisplay review={item.review} />
+						<li key={review._id}>
+							<p><Link to={`/library/${review.libraryItem._id}`}>{review.libraryItem.name} </Link>Review:</p>
+							<ReviewDisplay review={review} />
 						</li>
 					)
 				})}
