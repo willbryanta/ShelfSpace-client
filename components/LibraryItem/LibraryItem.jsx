55fed4a253bf4be6dd7d6a4f2c901cf075b353b2
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-11-05 16:07:31 +1100

    edit: added libraryItemId back to useEffect dependency array and reformatted how reviews render

---
+++ components/LibraryItem/LibraryItem.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useState, useParams} from 'react'
+import * as libraryItemService from '../../services/libraryItemService'
+import ReviewDisplay from '../ReviewDisplay/ReviewDisplay'
+
+function LibraryItem(props) {
+	const {user} = props
+	const {libraryItemId} = useParams()
+	const [libraryItem, setLibraryItem] = useState(null)
+
+	useEffect(() => {
+		const fetchLibraryItem = async () => {
+			if (libraryItemId) {
+				const item = await libraryItemService.getLibraryItem(libraryItemId)
+				setLibraryItem(item)
+			}
+		}
+		fetchLibraryItem()
+	}, [libraryItemId])
+
+	return (
+		<div>
+			<ul>
+				<li>Name: {libraryItem.name}</li>
+				<li>Description: {libraryItem.description}</li>
+				<li>Publication Date: {libraryItem.publicationDate}</li>
+				<li>Author: {libraryItem.author}</li>
+				<li>
+					Reviews:
+					{
+						<ul>
+							{libraryItem.reviews.map((review) => (
+								<li key={review._id}>
+									<ReviewDisplay review={review} user={user} />
+								</li>
+							))}
+						</ul>
+					}
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default LibraryItem
