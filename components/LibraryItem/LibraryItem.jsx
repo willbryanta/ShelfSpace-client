0180d5ae871a6b4400c12b1bf4f4df0ba14b6113
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-11-05 14:23:30 +1100

    create: GET individual library items from their library item id

---
+++ components/LibraryItem/LibraryItem.jsx
@@ -0,0 +1,31 @@
+import {useEffect, useState, useParams} from 'react'
+import {getLibraryItem} from '../../services/libraryItemService'
+
+function LibraryItem() {
+	const {libraryItemId} = useParams()
+	const [libraryItem, setLibraryItem] = useState(null)
+
+	useEffect(() => {
+		const fetchLibraryItem = async () => {
+			if (libraryItemId) {
+				const item = await getLibraryItem(libraryItemId)
+				setLibraryItem(item)
+			}
+		}
+		fetchLibraryItem()
+	}, [libraryItemId])
+
+	return (
+		<div>
+			<ul>
+				<li>Name: {libraryItem.name}</li>
+				<li>Description: {libraryItem.description}</li>
+				<li>Publication Date: {libraryItem.publicationDate}</li>
+				<li>Author: {libraryItem.author}</li>
+				<li>Reviews: {libraryItem.reviews}</li>
+			</ul>
+		</div>
+	)
+}
+
+export default LibraryItem
