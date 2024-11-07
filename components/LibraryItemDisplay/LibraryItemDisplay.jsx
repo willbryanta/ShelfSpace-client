96f73413dfd1e740fe347f69cc4b37d1e4529181
dorkykenny <kennethcarlsamoza8@gmail.com>
2024-11-08 09:13:39 +1100

    refactors: library item display, integrating placeholder image for poster

--- components/LibraryItemDisplay/LibraryItemDisplay.jsx
+++ components/LibraryItemDisplay/LibraryItemDisplay.jsx
@@ -1,15 +1,16 @@
-import {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import * as libraryItemService from '../../services/libraryItemService'
 import ReviewDisplay from '../ReviewDisplay/ReviewDisplay'
-import {format} from 'date-fns'
+import { format } from 'date-fns'
+import styles from './LibraryItem.module.css';
 
 const formatDate = (date) => {
 	return format(new Date(date), 'yyyy')
 }
 function LibraryItemDisplay(props) {
-	const {user, handleError} = props
-	const {libraryItemId} = useParams()
+	const { user, handleError } = props
+	const { libraryItemId } = useParams()
 	const [libraryItem, setLibraryItem] = useState({
 		name: '',
 		description: '',
@@ -37,21 +38,24 @@ function LibraryItemDisplay(props) {
 	return (
 
 		<>
-		<h1>{libraryItem.name}</h1>
-		<img src={`https://image.tmdb.org/t/p/w200/${libraryItem.posterPath}`}></img>
-		<p><strong>Overview:</strong> {libraryItem.description}</p>
-		<p><strong>Release Year:</strong> {formatDate(libraryItem.publicationDate)}</p>
-		<p><strong>Reviews:</strong></p>
-		<ul>
-							{libraryItem?.reviews?.map((review) => (
-								<li key={review._id}>
-									<ReviewDisplay review={review} user={user} />
-								</li>
-							)
-						)}
-					</ul>
+			<h1>{libraryItem.name}</h1>
+			{(libraryItem.posterPath) ?
+				<img src={`https://image.tmdb.org/t/p/w200/${libraryItem.posterPath}`} className={styles.poster}></img>
+				: <img src="https://placeholder.pics/svg/300x300/391C0B/391C0B" className={styles.poster}></img>
+			}
+			<p><strong>Overview:</strong> {libraryItem.description}</p>
+			<p><strong>Release Year:</strong> {formatDate(libraryItem.publicationDate)}</p>
+			<p><strong>Reviews:</strong></p>
+			<ul>
+				{libraryItem?.reviews?.map((review) => (
+					<li key={review._id}>
+						<ReviewDisplay review={review} user={user} />
+					</li>
+				)
+				)}
+			</ul>
 		</>
-		
+
 	)
 }
 
