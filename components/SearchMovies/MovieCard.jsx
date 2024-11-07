9cfe29e34fe0e0c470ab051c93a7726a0bfda2ab
dorkykenny <kennethcarlsamoza8@gmail.com>
2024-11-07 23:12:08 +1100

    refactors movieCard to adhere to new schema

--- components/SearchMovies/MovieCard.jsx
+++ components/SearchMovies/MovieCard.jsx
@@ -1,13 +1,32 @@
+import * as libraryItemService from '../../services/libraryItemService'
+import { useState } from 'react';
 import styles from './MovieCard.module.css';
 
 export default function MovieCard(props) {
-    const {movie} = props
+    const { movie, user } = props
+
+    const [movieAdded, setMovieAdded] = useState(false)
+
+    const addToLibrary = async () => {
+        const newMovie = {
+            posterPath: movie.poster_path,
+            name: movie.title,
+            description: movie.overview,
+            publicationDate: movie.release_date,
+            author: user
+        }
+
+        await libraryItemService.createLibraryItem(newMovie)
+        setMovieAdded(true)
+    }
 
     return (
-            <li className={styles.movie}>
-                <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} className={styles.moviePoster}></img>
-                <p className={styles.movieTitle}>{movie.title}</p>
-                <button>+</button>
-            </li>
+        <li className={styles.movie}>
+            <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} className={styles.moviePoster}></img>
+            <p className={styles.movieTitle}>{movie.title}</p>
+            {!movieAdded ? (<button onClick={addToLibrary}>+</button>)
+                :
+                (<button>✔️</button>)}
+        </li>
     )
 }
