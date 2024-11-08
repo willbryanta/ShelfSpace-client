4c29f7ee68c6a9242e831b02f066d2f99ccecef4
dorkykenny <kennethcarlsamoza8@gmail.com>
2024-11-08 11:06:47 +1100

    fixes: button to add to library in search

--- components/SearchMovies/MovieCard.jsx
+++ components/SearchMovies/MovieCard.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import * as libraryItemService from '../../services/libraryItemService'
 
 import styles from './MovieCard.module.css';
@@ -5,8 +6,12 @@ import styles from './MovieCard.module.css';
 export default function MovieCard(props) {
     const { movie, user, libraryItems } = props
 
-    // const [movieAdded, setMovieAdded] = useState(false)
-    const isMovieAdded = libraryItems.find((item) => item.description === movie.overview)
+    const [movieAdded, setMovieAdded] = useState(false)
+    
+    useEffect(() => {
+        const isMovieInLibrary = libraryItems.some((item) => item.description === movie.overview);
+        setMovieAdded(isMovieInLibrary);
+    }, [libraryItems, movie.overview]);
 
     const addToLibrary = async () => {
         const newMovie = {
@@ -18,13 +23,14 @@ export default function MovieCard(props) {
         }
 
         await libraryItemService.createLibraryItem(newMovie)
+        setMovieAdded(true)
     }
 
     return (
         <li className={styles.movie}>
             <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} className={styles.moviePoster}></img>
             <p className={styles.movieTitle}>{movie.title}</p>
-            {isMovieAdded ? (<button className={styles.addToLib}>✔️</button>)
+            {movieAdded ? (<button className={styles.addToLib}>✔️</button>)
                 :
                 (<button onClick={addToLibrary} className={styles.addToLib}>Add to Library</button>)}
         </li>
